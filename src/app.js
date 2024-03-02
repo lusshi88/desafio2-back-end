13b3e49d4c7da6f0c4ee1d8ef64afd707c66bc57
Martín Precelle <tinchoprecelle@gmail.com>
2024-03-02 13:35:33 -0300

    Partial fix

--- src/app.js
+++ src/app.js
@@ -1,66 +1,58 @@
-const express = require("express");
-const productsRoutes =  require('./routes/products.js');
-const cartRoutes = require('./routes/carts.js');
-const viewsRoutes = require('./routes/views.js');
-const path = require('path');
-const httpServer = require('http');
-const displayRoutes = require('express-routemap');
-
+const express = require('express')
+const productsRoutes = require('./routes/products.js')
+const cartRoutes = require('./routes/carts.js')
+const viewsRoutes = require('./routes/views.js')
+const handlebars = require('express-handlebars')
+const path = require('path')
+const httpServer = require('http')
+const displayRoutes = require('express-routemap')
 
 // MONGODB --------------------------------
-const mongoose  = require ("mongoose")
-mongoose.connect('mongodb+srv://ecommerce88:UWfDP0RRKUB5Oofa@cluster88.gxgjzbs.mongodb.net/?retryWrites=true&w=majority', {
+const mongoose = require('mongoose')
+mongoose
+  .connect('mongodb+srv://ecommerce88:UWfDP0RRKUB5Oofa@cluster88.gxgjzbs.mongodb.net/?retryWrites=true&w=majority', {
     dbName: 'ecommerce',
   })
-    .then((conn) => {
-        console.log("CONNECTED TO MONGODB ")
-    })
-    .catch((err) => {
-        console.log("ERROR CONNECTING TO DB", err)
-    })
-
-
+  .then((conn) => {
+    console.log('CONNECTED TO MONGODB ')
+  })
+  .catch((err) => {
+    console.log('ERROR CONNECTING TO DB', err)
+  })
 
 // puerto del server --------------------------------
-const PORT = 8080;
+const PORT = 8080
 
-const app = express();
+const app = express()
 
 // Middleware para analizar cuerpos de solicitud JSON
-app.use(express.json());
+app.use(express.json())
 
+// Handlebars config
+app.engine('handlebars', handlebars.engine())
+app.set('views', __dirname + '/views')
+app.set('view engine', 'handlebars')
 
-// productos de mi JSON
-const productos = require ("./data/productos.json");
-
-app.use(express.json());
-// app.use(express.urlencoded());
-
-
-const API_PREFIX = "api";
+const API_PREFIX = 'api'
 
 // archivo estatico
-app.use(`/static`,express.static(`public`));
+app.use(`/static`, express.static(`public`))
 
-
-app.use(`/${API_PREFIX}/products`, productsRoutes);
-app.use(`/${API_PREFIX}/carts`, cartRoutes);
-app.use(`/${API_PREFIX}/views`, viewsRoutes);
-
-
-
-
+app.use(`/${API_PREFIX}/products`, productsRoutes)
+app.use(`/${API_PREFIX}/carts`, cartRoutes)
+app.use(`/${API_PREFIX}/views`, viewsRoutes)
 
 // Codigo para iniciar el server ----------------
 app.listen(PORT, () => {
   displayRoutes(app)
-  console.log("Server en funcionamiento");
-});
+  console.log('Server en funcionamiento')
+})
 
-
-app.get ("/"),(req, res) => {
-  res.render("index.js")
-}
+app.get('/'),
+  (req, res) => {
+    res.render('index.js')
+  }
 
 module.exports = {
-  httpServer };
+  httpServer,
+}
--- src/routes/views.js
+++ src/routes/views.js
@@ -1,22 +1,13 @@
-const express = require('express');
+const express = require('express')
 
-const router = express.Router();
+const router = express.Router()
 
-const productsModel = require ('../models/productsModel.js');
+const productsModel = require('../models/productsModel.js')
 
+router.get('/products', async (req, res) => {
+  const { page = 1 } = req.params
+  const products = await productsModel.paginate({}, { limit: 10, page, lean: true })
+  res.render('products', { products: products.docs })
+})
 
-router.get ("/products ",async (req, res) => {
-      const {page = 1 } = req.params;
-      const {docs,hasNextPage,nextPage,prevPage} = req.query
-      await productsModel.paginate ({}, {limit:10,page,lean:true},)
-      res.render("products",{
-        students: docs,
-        page,
-        hasNextPage,
-        nextPage,
-        prevPage
-      })
-});
-
-
-module.exports = router;
+module.exports = router
