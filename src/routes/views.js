0cd1ce65442f0ff3c729be8bc1e9815e65b34ea9
Luis <bravoluis439@ymail.com>
2024-03-05 15:15:46 -0300

    se modifican errores

--- src/routes/views.js
+++ src/routes/views.js
@@ -1,19 +1,23 @@
-const express = require('express');
+const express = require('express')
 
-const router = express.Router();
+const router = express.Router()
 
-const productsModel = require ('../models/productsModel.js');
+const productsModel = require('../models/productsModel.js')
 
-
-router.get ("/products",async (req, res) => {
+router.get ("/products ",async (req, res) => {
       const {page = 1 } = req.params;
-      const products = await productsModel.paginate ({}, {limit:10 ,page, lean: true},)
-      res.render('products', { products: products.docs })
+      const {docs,hasNextPage,nextPage,prevPage} = req.query
+      await productsModel.paginate ({}, {limit:10,page,lean:true},)
+      res.render("products",{
+        students: docs,
+        page,
+        hasNextPage,
+        nextPage,
+        prevPage
+      })
 });
 
 
-        
 
 
-
-module.exports = router;
+module.exports = router
